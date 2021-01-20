76c316ab3703e93407ae0cd16ca4a22b71fd2e45
ElHuaco <alexhuac@gmail.com>
2021-01-20 13:21:34 +0100

    Esbozando. Simultaneous start done. Need status retrieval using pthread_join.

--- philo_one/main.c
+++ philo_one/main.c
@@ -6,13 +6,12 @@
 /*   By: aleon-ca <aleon-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 17:40:30 by aleon-ca          #+#    #+#             */
-/*   Updated: 2021/01/19 18:31:39 by aleon-ca         ###   ########.fr       */
+/*   Updated: 2021/01/20 13:20:10 by aleon-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
-//Hay que pasar id pero también los args de num_phi, time_to_die etc.
-//Sin hacer copias del struct innecesarias.
+
 static void		*primum_vivere(void *philo_id)
 {
 	unsigned long		id;
@@ -20,13 +19,13 @@ static void		*primum_vivere(void *philo_id)
 	id = *(unsigned long *)(&philo_id);
 //gettimeofday aquí y si no ha comido en time_to_die, end
 	pthread_mutex_lock(&g_mutex_forks);
-printf("Hola Mundo, soy el Filósofo %lu\n", id);
+printf("Hola mundo, soy el Filósofo %lu\n", id);
 	if ((id != 0)
 		&& (g_forks[id - 1] == 1) && (g_forks[id] == 1))
 	{
-		g_forks[id] = 0;
 		g_forks[id - 1] = 0;
-printf("\tÑam Ñam\n");
+		g_forks[id] = 0;
+printf("\tñam ñam ñam\n");
 	//Deinde Filosofare
 	}
 	else if ((id == 0)
@@ -34,12 +33,13 @@ printf("\tÑam Ñam\n");
 	{
 		g_forks[g_args.num_phi - 1] = 0;
 		g_forks[0] = 0;
-printf("\tÑam Ñam\n");
+printf("\tñam ñam ñam\n");
 	//Deinde filosofare
 	}
 	else
 printf("\t¡Que me quedo sin comer!\n");
 	pthread_mutex_unlock(&g_mutex_forks);
+//usleep(g_args.time_to_sleep);
 	return (NULL);
 }
 
@@ -61,6 +61,22 @@ static int		set_and_check_args(int argc, char **argv, t_thread_data *args)
 			|| !(args->time_to_eat) || !(args->time_to_sleep));
 }
 
+static int		create_threads(pthread_t *phi_threads, unsigned long *phi_id)
+{
+	unsigned long		i;
+
+	i = 0;
+	while (i < g_args.num_phi)
+	{
+		phi_id[i] = i;
+		if ((pthread_create(&phi_threads[i], NULL, primum_vivere,
+			(void *) phi_id[i])))
+			return (1);
+		++i;
+	}
+	return (0);
+}
+
 static void		garbage_collection(pthread_t **philos, unsigned long **id)
 {
 	pthread_mutex_destroy(&g_mutex_forks);
@@ -75,26 +91,27 @@ int				main(int argc, char **argv)
 	unsigned long	*phi_id;
 	pthread_t		*phi_threads;
 
-	g_forks = NULL;
 	if (((argc != 5) && (argc != 6))
 		|| (set_and_check_args(argc, argv, &g_args))
 		|| !(g_forks = malloc(sizeof(char) * g_args.num_phi))
 		|| !(memset(g_forks, 1,  sizeof(char) * g_args.num_phi))
 		|| !(phi_threads = malloc(sizeof(pthread_t) * g_args.num_phi))
-		|| !(phi_id = malloc(sizeof(unsigned long) * g_args.num_phi)))
+		|| !(phi_id = malloc(sizeof(unsigned long) * g_args.num_phi))
+		|| (pthread_mutex_init(&g_mutex_forks, NULL))
+		|| (pthread_mutex_lock(&g_mutex_forks))
+		|| (create_threads(phi_threads, phi_id)))
 		return (1);
-	pthread_mutex_init(&g_mutex_forks, NULL);
+	pthread_mutex_unlock(&g_mutex_forks);
 	i = 0;
-	while (i++ < g_args.num_phi)
-	{
-		phi_id[i] = i;
-		if ((pthread_create(&phi_threads[i], NULL, primum_vivere,
-			(void *) phi_id[i])) || (pthread_join(phi_threads[i], NULL)))
+	while (i < g_args.num_phi)
+		if ((pthread_join(phi_threads[i++], NULL)))
 			return (1);
-	}
 	i = 0;
-	while (i++ < g_args.num_phi)
+	while (i < g_args.num_phi)
+	{
 		printf("fork #%lu: %d\n", i, g_forks[i]);
+		++i;
+	}
 	garbage_collection(&phi_threads, &phi_id);
 	return (0);
 }
