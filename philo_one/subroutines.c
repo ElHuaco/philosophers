dd0a1f3fa592fc49d9815aab500576adcb741dda
ElHuaco <alexhuac@gmail.com>
2021-01-21 19:50:22 +0100

    Pseudocodeando.

--- philo_one/subroutines.c
+++ philo_one/subroutines.c
@@ -6,11 +6,35 @@
 /*   By: aleon-ca <aleon-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 13:13:18 by aleon-ca          #+#    #+#             */
-/*   Updated: 2021/01/21 13:48:21 by aleon-ca         ###   ########.fr       */
+/*   Updated: 2021/01/21 19:49:44 by aleon-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
+
+static void	print_status_change(struct timeval *t, unsigned long id, char *str)
+{
+	unsigned long double		time;
+	char						*time_str;
+	char						*id_str;
+
+//Importancia de ultoa, uldtoa eficaces -> ∆t <= 10 ms entre muerte y print.
+	gettimeofday(t + 1, NULL);
+	t[1].tv_sec -= t[0].tv_sec;
+	t[1].tv_usec -= t[0].tv_usec;
+	time = t[1].tv_sec * 1000.0 + t[1].tv_usec / 1000.0;
+	time_str = ft_uldtoa(time);
+	id_str = ft_ultoa(id);
+	pthread_mutex_lock(&g_mutex_stdout);
+	write(1, time_str, ft_strlen(time_str));
+	write(1, " ", 1);
+	write(1, id_str, ft_strlen(id_str));
+	write(1, " ", 1);
+	write(1, str, ft_strlen(str));
+	pthread_mutex_unlock(&g_mutex_stdout);
+	free(time_str);
+	free(id_str);
+}
 
 void		*primum_vivere(void *philo_id)
 {
